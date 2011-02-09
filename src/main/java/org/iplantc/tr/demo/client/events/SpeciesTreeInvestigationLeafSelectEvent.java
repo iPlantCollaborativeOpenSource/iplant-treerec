c8c4e0d4083bdcaf2409e7aef00dcbc87cfde79f
sriram srinivasan <sriram@dhcp-96870a20.ece.arizona.edu>
2011-02-09 13:34:29 -0700

    add events for leaf selection

---
+++ src/main/java/org/iplantc/tr/demo/client/events/SpeciesTreeInvestigationLeafSelectEvent.java
@@ -0,0 +1,41 @@
+package org.iplantc.tr.demo.client.events;
+
+import java.util.ArrayList;
+
+import com.google.gwt.event.shared.GwtEvent;
+
+public class SpeciesTreeInvestigationLeafSelectEvent extends
+		GwtEvent<SpeciesTreeInvestigationLeafSelectEventHandler>
+{
+
+	public static final GwtEvent.Type<SpeciesTreeInvestigationLeafSelectEventHandler> TYPE = new GwtEvent.Type<SpeciesTreeInvestigationLeafSelectEventHandler>();
+	
+	private ArrayList<Integer> geneTreeNodesToSelect;
+	
+	/**
+	 * @return the geneTreeNodesToSelect
+	 */
+	public ArrayList<Integer> getGeneTreeNodesToSelect()
+	{
+		return geneTreeNodesToSelect;
+	}
+
+	public SpeciesTreeInvestigationLeafSelectEvent(ArrayList<Integer> geneTreeNodesToSelect)
+	{
+		this.geneTreeNodesToSelect = geneTreeNodesToSelect;
+	}
+
+	@Override
+	protected void dispatch(SpeciesTreeInvestigationLeafSelectEventHandler handler)
+	{
+		handler.onFire(this);
+
+	}
+
+	@Override
+	public com.google.gwt.event.shared.GwtEvent.Type<SpeciesTreeInvestigationLeafSelectEventHandler> getAssociatedType()
+	{
+		return TYPE;
+	}
+
+}
